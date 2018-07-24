b8c553cd3198de09aa2fcd1f78219cc38fe865b2
evilwangyang <evilwangyang@126.com>
2018-07-24 12:53:40 +0800

    kNN_test2.py中增加了file2matrix函数

--- kNN_test2.py
+++ kNN_test2.py
@@ -9,3 +9,26 @@
 # @Software  :  PyCharm
 
 # ********************************************************* 
+import numpy as np
+
+def file2matrix(filename):
+	fr = open(filename)
+	arrayOLines = fr.readlines()
+	numberOfLines = len(arrayOLines)
+	returnMat = np.zeros((numberOfLines,3))
+	classLabelVector = []
+	index = 0
+	for line in arrayOLines:
+		line = line.strip()
+		listFromLine = line.split('\t')
+		returnMat[index,:] = listFromLine[0:3]
+		classLabelVector.append(int(listFromLine[-1]))
+		index += 1
+	return returnMat,classLabelVector
+
+if __name__ == '__main__':
+
+	filename = "datingTestSet2.txt"
+	datingDataMat,datingLabels = file2matrix(filename)
+	print(datingDataMat)
+	print(datingLabels)
